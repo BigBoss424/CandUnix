045819dece7b980da48237d9a4f8650826abfaf8
bigboss424 <ajones061392@gmail.com>
2014-10-03 14:47:13 -0700

    Added Lab2 script.

---
+++ Labs/Lab_2/lab2.sh
@@ -0,0 +1,68 @@
+#CSCD 240: C and Unix Lab 2 Script
+#Created by: Aaron Jones
+#Date: 10/3/2014
+#Description: This script will be used to run all the commands
+#in the lab 2 assignment. The question will be displayed first
+#followed by the command, and then followed by the answer if required.
+#Feel free to email me if you run into any issues with the way the script
+#runs, at ajones061392@gmail.com.
+
+echo "CSCD 240: C and Unix Lab 2 Script"
+echo -e "\n"
+echo "Created by: Aaron Jones"
+echo "Date: 10/3/2014"
+echo "========================================================================"
+echo -e "\n"
+echo "Description: This script will be used to run all the commnds"
+echo "in the lab 2 assignment. The question will be displayed first"
+echo "followed by the command, and then followed by the answer if required."
+echo "Feel free to email me if you run into any issues with the way the script"
+echo "runs, at ajones061392@gmail.com."
+echo -e "\n"
+echo "========================================================================"
+
+echo -e "\n"
+echo "========================================================================"
+echo "1. Clearly explain why programs should be placed in /bin or /usr/bin"
+echo "========================================================================"
+echo -e "\n"
+
+echo "========================================================================"
+echo "Answer: "
+echo "========================================================================"
+
+echo -e "\n"
+echo "========================================================================"
+echo "2. You are asked to use a program named mystery which you have never used"
+echo "before. Explain how you would find information on the program and what it"
+echo "does. List all the ways you know."
+echo "========================================================================"
+echo -e "\n"
+
+echo "========================================================================"
+echo "Answer: "
+echo "========================================================================"
+
+echo -e "\n"
+echo "========================================================================"
+echo "3. There are many other environments variables avaible to the user. "
+echo "Capture the printenv command. Describe 3 of the environment variables."
+echo "========================================================================"
+echo -e "\n"
+
+echo "Running printenv command....."
+printenv
+
+echo -e "\n"
+echo "========================================================================"
+echo "Answer: "
+echo "========================================================================"
+
+echo -e "\n"
+echo "========================================================================"
+echo "4) Capture the out of the file command on the chmod executable. (Where"
+echo "does chmod live?) Explain the information being displayed."
+echo "========================================================================"
+echo -e "\n"
+
+
