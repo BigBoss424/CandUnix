6fb43e1bc45fdd937c960cdf0e3fe394d201a942
bigboss424 <ajones061392@gmail.com>
2014-10-28 12:02:34 -0700

    Added the compelete client.c file have yet to compile it. Will add the compiled file, after compiling.

--- Examples/client.c
+++ Examples/client.c
@@ -2,14 +2,62 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
-#include <Lunistd.h>
+#include <unistd.h>
 
+#define DATA "Hello World of Sockets"
 int main(int argc, char *argv[]);
 {
 
+	/*Variable*/
+	int sock; //socket
+	struct sockaddr_in server;
+	char buff[1024]; //buffer
+	struct hostent *hp;
+	/*Create socket*/
+	sock = sock(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0)
+	{
+		perror("Failed to create socket!");
+		close(sock);
+		exit(1);
+	}
 	
+	//Name the server
+	
+	server.sin_family = AF_INET;
+	
+	hp = gethostbyname(*argv[1]);
+	if(hp == 0)
+	{
+		perror("Failed to get host binding");
+		close(sock);
+		exit(1);
+	}
+
+	memcpy(&server.sin_addr, hp->h_addr, hp->h_length);
+	server.port = htona(5000);
+
+	//connect socket
+
+	if( connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0)
+	{
+		perror("Connection failed");
+		close(sock);
+		exit(1);
+	}  
+
+	if( send(sock, DATA, sizeof(DATA), 0) < 0)
+	{
+		perror("Send error");
+		close(sock);
+		exit(1);
+	}  
+
+	printf("Send %s", DATA);
+	close(sock);
 
 
 
