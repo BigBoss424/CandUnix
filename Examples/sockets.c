791d7bac9abd67214544560a088d800b140c84fa
bigboss424 <ajones061392@gmail.com>
2014-10-28 12:13:46 -0700

    Fixed the error in the sockets.c (server) file, in which I forgot to use htons when stating the port number. Which would result in the error of connection refused.

--- Examples/sockets.c
+++ Examples/sockets.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = 5000;
+	server.sin_port = htons(5000);
 	
 	/*Call bind */
 
