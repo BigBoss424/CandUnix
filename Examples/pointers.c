bc352b1b5d6d8102def776ef48f5ee2649840945
bigboss424 <ajones061392@gmail.com>
2014-10-28 12:24:45 -0700

    Created a pointers example for code reference. Will also be documenting the memory map of this code as well for reference of drawing out memory maps.

---
+++ Examples/pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	/*Before manipulation with pointers*/
+	int n;
+
+	n = 25;
+	
+	printf("n is: &d\n", n);
+
+	/*After manipulation with pointers*/
+	//int *k;
+	//k = &n;
+
+	//printf(*k);
+	
+	return 0;
+}
