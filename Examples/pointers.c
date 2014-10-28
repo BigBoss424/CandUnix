8f90de4e3792c82d325bc820bcf5a958402741d0
Aaron Jones <BigBoss424@hotmail.com>
2014-10-28 13:06:42 -0700

    Pointer Array
    Added the example for pointers in an Array. Can’t get past this error
    with the printing.

--- Examples/pointers.c
+++ Examples/pointers.c
@@ -4,16 +4,18 @@ int main(int argc, char *argv[])
 {
 	/*Before manipulation with pointers*/
 	int n;
+	int *k;
 
 	n = 25;
+	k = &n;
 	printf("Before manipulation with pointers \n");	
 	printf("n is: %d\n", n);
 
 	/*After manipulation with pointers*/
-	//int *k;
 	//k = &n;
-	//printf("After manipulation with pointers \n");
-	//printf(*k);
+	printf("After manipulation with pointers \n");
+	printf("K is located at: %x\n", n);
+	printf("K is: %d\n", *k);
 	
 	return 0;
 }
