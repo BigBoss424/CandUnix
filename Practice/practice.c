0f098a8e249d3abd10383dd8d4b17e1feaa0c1b1
Aaron Jones <BigBoss424@hotmail.com>
2014-11-02 16:42:19 -0800

    This is a practice for pointer.

---
+++ Practice/practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int m;
+    int *pointer;
+
+    //Creating this program to get more practice with pointers.
+
+    //First will be the simple manipulation with pointers.
+
+    m = 10;
+
+    *pointer = m;
+
+    printf("This is the value for m: %d", pointer);
+
+}
