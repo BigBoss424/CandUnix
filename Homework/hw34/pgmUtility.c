4c90d310f7b6c0bf715a170672f9597019afba06
bigboss424 <ajones061392@gmail.com>
2014-11-18 11:04:24 -0800

    Added the files for Homework 34 didn't complete it in time though.

--- Homework/hw34/pgmUtility.c
+++ Homework/hw34/pgmUtility.c
@@ -10,7 +10,30 @@
 // NOTE: You can NOT change the input, output, and argument type of the functions in pgmUtility.h
 // NOTE: You can NOT change the prototype (signature) of any functions listed in pgmUtility.h
 
+int ** pgmRead(char **header, int *numRows, int*numCols, FILE *in)
+{
 
+}
+
+int pgmDrawCircle(int **pixels, int numRows, int numCols, int centerRow, int centerCol, int radius, char**header)
+{
+
+}
+
+int pgmDrawEdge(int **pixelsl, int numRows, int numCols, int edgewidth, char **header)
+{
+
+}
+
+int pgmWrite(const char **header, const int **pixels, int numRows, int numCols, FILE *out)
+{
+
+}
+
+double distance(int p1[], int p2[])
+{
+
+}
 int programDescription()
 {
     time_t current_time;
